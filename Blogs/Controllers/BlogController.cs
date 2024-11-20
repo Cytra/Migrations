be1a04e9a4b75d5da910683bc9b68d8b366b8c01
Martynas Samuilovas <martynas.samuilovas@samdo.com>
2024-11-20 09:28:14 +0200

    update

--- Blogs/Controllers/BlogController.cs
+++ Blogs/Controllers/BlogController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System;
+using ChromiumHtmlToPdfLib;
+using ChromiumHtmlToPdfLib.Settings;
 
 namespace Blogs.Controllers;
 
@@ -7,10 +10,34 @@ namespace Blogs.Controllers;
 [Route("[controller]")]
 public class BlogController() : ControllerBase
 {
-    [Authorize(Policy = "ReadAccess")]
+    //[Authorize(Policy = "ReadAccess")]
     [HttpGet("secure-data")]
     public IActionResult GetSecureData()
     {
         return Ok("You have read access!");
+    }
+
+    [HttpPost("generate")]
+    public async Task<IActionResult> GeneratePdf([FromBody] string htmlContent)
+    {
+        var converter = new PdfService();
+        var pdfBytes = await converter.GeneratePdfAsync(htmlContent);
+        return File(pdfBytes, "application/pdf", "generated.pdf");
+    }
+}
+
+
+public class PdfService
+{
+
+    public async Task<byte[]> GeneratePdfAsync(string htmlContent)
+    {
+        await using var converter = new Converter();
+        converter.AddChromiumArgument("--no-sandbox");
+        converter.AddChromiumArgument("--disable-dev-shm-usage");
+        await using var pdfStream = new MemoryStream();
+        await converter.ConvertToPdfAsync(htmlContent, pdfStream, new PageSettings());
+        var result = pdfStream.ToArray();
+        return result;
     }
 }
--- Blogs/Program.cs
+++ Blogs/Program.cs
@@ -9,25 +9,25 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-    .AddJwtBearer(options =>
-    {
-        options.Authority = "http://localhost:8080/realms/local-dev";
-        options.Audience = "blogs";
-        options.RequireHttpsMetadata = false;  // Disable for local dev
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateAudience = true,
-            ValidateIssuer = true
-        };
-    });
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//    .AddJwtBearer(options =>
+//    {
+//        options.Authority = "http://localhost:8080/realms/local-dev";
+//        options.Audience = "blogs";
+//        options.RequireHttpsMetadata = false;  // Disable for local dev
+//        options.TokenValidationParameters = new TokenValidationParameters
+//        {
+//            ValidateAudience = true,
+//            ValidateIssuer = true
+//        };
+//    });
 
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("ReadAccess", policy => policy.RequireAssertion(context =>
-        context.User.HasClaim(c => c.Type == "scope" && c.Value.Contains("blogs:view"))
-    ));
-});
+//builder.Services.AddAuthorization(options =>
+//{
+//    options.AddPolicy("ReadAccess", policy => policy.RequireAssertion(context =>
+//        context.User.HasClaim(c => c.Type == "scope" && c.Value.Contains("blogs:view"))
+//    ));
+//});
 
 var app = builder.Build();
 
